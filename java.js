6d13d04996db5a8bd705450abddd10117075b0d8
KDiyorbek <diyorbek@duck.com>
2024-08-31 00:17:20 +0500

    upd +

--- java.js
+++ java.js
@@ -14,9 +14,20 @@ function checkPassword() {
       hideLockScreen();
       // Redirect to the main content or perform other actions
   } else {
-      alert("❌ incorrect password.  try another one");
+      alert("❌ incorrect password.");
   }
 }
 
 // Show the lock screen on page load
 window.onload = showLockScreen;
+
+// Add event listener to password input field
+var passwordInput = document.getElementById("password");
+passwordInput.addEventListener("input", function() {
+  var button = document.querySelector(".lock-screen-content button");
+  if (passwordInput.value !== "") {
+      button.style.display = "block";
+  } else {
+      button.style.display = "none";
+  }
+});
